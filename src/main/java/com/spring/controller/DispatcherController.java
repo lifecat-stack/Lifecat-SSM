6553fb585227e76c2ff0a4d80359e589a1bf4814
kevinten10 <596823919@qq.com>
2018-07-11 13:18:07 +0800

    version2.0   --2018.7.10

--- src/main/java/com/spring/controller/DispatcherController.java
+++ src/main/java/com/spring/controller/DispatcherController.java
@@ -6,15 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/")
 public class DispatcherController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         return "index";
+    }
+
+    @RequestMapping(value = "/userhome", method = RequestMethod.GET)
+    public String userhome() {
+        return "userhome";
     }
 
 }
