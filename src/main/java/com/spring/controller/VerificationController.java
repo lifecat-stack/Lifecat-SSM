ffc37a264b2e621657f9957db8bd59a96e8bec5b
wshten <wshten@gmail.com>
2018-08-03 10:10:50 +0800

    version2.1.0   --2018.8.3   --开发文档
    Signed-off-by: wshten <wshten@gmail.com>

--- src/main/java/com/spring/controller/VerificationController.java
+++ src/main/java/com/spring/controller/VerificationController.java
@@ -25,27 +25,27 @@ public class VerificationController {
     public VerificationController(AlertService alertService) {
         this.alertService = alertService;
     }
-
-    /**
-     * admin login
-     */
-    @RequestMapping(value = "/admin/login", method = RequestMethod.POST)
-    public String adminLogin() {
-        System.out.println("admin login verification");
-        String adminName = "admin";
-        String adminPassword = "1234";
-        // verify
-        UsernamePasswordToken token = new UsernamePasswordToken(adminName, adminPassword);
-        Subject subject = SecurityUtils.getSubject();
-        try {
-            subject.login(token);
-            // success: send message
-//            AlertMessage message = new AlertMessage("admin login success");
-//            userAlertService.sendLoginAlert(message);
-            return "home";
-        } catch (Exception e) {
-            return "redirect:/index.html";
-        }
-    }
+//
+//    /**
+//     * admin login
+//     */
+//    @RequestMapping(value = "/admin/login", method = RequestMethod.POST)
+//    public String adminLogin() {
+//        System.out.println("admin login verification");
+//        String adminName = "admin";
+//        String adminPassword = "1234";
+//        // verify
+//        UsernamePasswordToken token = new UsernamePasswordToken(adminName, adminPassword);
+//        Subject subject = SecurityUtils.getSubject();
+//        try {
+//            subject.login(token);
+//            // success: send message
+////            AlertMessage message = new AlertMessage("admin login success");
+////            userAlertService.sendLoginAlert(message);
+//            return "home";
+//        } catch (Exception e) {
+//            return "redirect:/index.html";
+//        }
+//    }
 
 }
